73c39e654cd4a08d785ccd351cbd964ea71b8f95
Bessemouelhi <bessem.mouelhi@outlook.fr>
2023-07-13 11:50:53 +0200

    improve design

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 radius: 50,
@@ -34,10 +35,101 @@ class MyApp extends StatelessWidget {
                   fontFamily: 'Source Sans Pro',
                   fontSize: 28,
                   fontWeight: FontWeight.bold,
+                  color: Colors.teal.shade200,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
                   color: Colors.white,
                 ),
               ),
-              Container()
+              Container(
+                padding: EdgeInsets.all(15),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.add_shopping_cart,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      '+33 6 12 34 56 78',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(15),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'bessem.mouelhi@flutter.com',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              // Card(
+              //   child: Column(
+              //     children: [
+              //       ListTile(
+              //         title: const Text(
+              //           '1625 Main Street',
+              //           style: TextStyle(fontWeight: FontWeight.w500),
+              //         ),
+              //         subtitle: const Text('My City, CA 99984'),
+              //         leading: Icon(
+              //           Icons.restaurant_menu,
+              //           color: Colors.blue[500],
+              //         ),
+              //       ),
+              //       const Divider(),
+              //       ListTile(
+              //         title: const Text(
+              //           '(408) 555-1212',
+              //           style: TextStyle(fontWeight: FontWeight.w500),
+              //         ),
+              //         leading: Icon(
+              //           Icons.contact_phone,
+              //           color: Colors.blue[500],
+              //         ),
+              //       ),
+              //       ListTile(
+              //         title: const Text('costa@example.com'),
+              //         leading: Icon(
+              //           Icons.contact_mail,
+              //           color: Colors.blue[500],
+              //         ),
+              //       ),
+              //     ],
+              //   ),
+              // ),
             ],
           ),
         ),
