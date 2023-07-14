4ec646f937fbb392c6f277cddc4662159eae46db
Bessemouelhi <bessem.mouelhi@outlook.fr>
2023-07-14 13:46:37 +0200

    create xylophone project

---
+++ xylophone/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(XyloApp());
+}
+
+class XyloApp extends StatelessWidget {
+  const XyloApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.deepPurpleAccent,
+        body: SafeArea(
+          child: Container(),
+        ),
+      ),
+    );
+  }
+}
