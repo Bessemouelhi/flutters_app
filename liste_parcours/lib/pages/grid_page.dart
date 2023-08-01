3676f982f62e5234b757adf63ca4043bc7afb823
Bessemouelhi <bessem.mouelhi@outlook.fr>
2023-08-02 00:20:28 +0200

    Update grid_page.dart

--- liste_parcours/lib/pages/grid_page.dart
+++ liste_parcours/lib/pages/grid_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../model/data_source.dart';
+
 class GridPage extends StatefulWidget {
   const GridPage({super.key});
 
@@ -10,8 +12,46 @@ class GridPage extends StatefulWidget {
 class _GridPageState extends State<GridPage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Grille'),
+    final datas = DataSource().allPlaces();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text('Grid List'),
+      ),
+      body: GridView.builder(
+        gridDelegate:
+            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+        itemBuilder: (ctx, i) {
+          final place = datas[i];
+          return InkWell(
+            child: Card(
+              elevation: 8,
+              child: Container(
+                padding: const EdgeInsets.all(5),
+                height: size.height * 0.75,
+                child: Column(
+                  children: [
+                    Expanded(
+                        child: Image.asset(place.getFolderPath(),
+                            fit: BoxFit.cover)),
+                    Text(
+                      place.name,
+                      style: const TextStyle(
+                          color: Colors.blue, fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            onTap: () {
+              //Naviguer
+              //NavigatorHelper().toDetail(context: context, place: place);
+            },
+          );
+        },
+        itemCount: datas.length,
+      ),
     );
   }
 }
